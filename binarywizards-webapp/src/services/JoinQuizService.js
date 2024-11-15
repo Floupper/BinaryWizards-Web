e362a8c6d1c3da026fe8e13ff3222beeac4aa046
Louis HERTSCHUH <louis.hertschuh@etu.unistra.fr>
2024-11-15 14:58:21 +0100

    feature: Add the 2 container of join party

--- binarywizards-webapp/src/services/JoinQuizService.js
+++ binarywizards-webapp/src/services/JoinQuizService.js
@@ -1,9 +1,10 @@
 import config from '../config';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-export const checkQuizExists = async (gameCode) => {
+export const checkGameExists = async (gameCode) => {
   try {
     const response = await fetch(`${config.API_BASE_URL}quiz/${gameCode}/question`);
+    console.log(response);
     if (!response.ok) {
       toast.error('Quiz not found');
     }
@@ -13,3 +14,17 @@ export const checkQuizExists = async (gameCode) => {
     //throw error;
   }
 };
+
+export const createGameWithQuizId = async (quizId) => {
+  try {
+    const response = await fetch(`${config.API_BASE_URL}game/${quizId}/create`);
+    if (!response.ok) {
+      toast.error('Quiz not found');
+    }
+    return response.json();
+  } catch (error) {
+    toast.error('Error checking quiz:', error);
+    //throw error;
+  }
+};
+
