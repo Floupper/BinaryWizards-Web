da02cede7e5fe10f04d192a96876cf256d8b045d
HERTSCHUH LOUIS <louis.hertschuh@etu.unistra.fr>
2024-11-20 08:55:38 +0000

    fix: remove console.log in  JoinQuizService.js

--- binarywizards-webapp/src/services/JoinQuizService.js
+++ binarywizards-webapp/src/services/JoinQuizService.js
@@ -4,7 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 export const checkGameExists = async (gameCode) => {
   try {
     const response = await fetch(`${config.API_BASE_URL}quiz/${gameCode}/question`);
-    console.log(response);
+
     if (!response.ok) {
       throw new Error('Quiz not found');
     }
