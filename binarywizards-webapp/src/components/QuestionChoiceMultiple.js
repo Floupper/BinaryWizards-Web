e80ee0537adab288c303dffad9692eb155706c29
Louis HERTSCHUH <louis.hertschuh@etu.unistra.fr>
2024-11-20 16:50:14 +0100

    fix: new version of API

--- binarywizards-webapp/src/components/QuestionChoiceMultiple.js
+++ binarywizards-webapp/src/components/QuestionChoiceMultiple.js
@@ -1,33 +1,42 @@
 import '../assets/QuestionChoiceMultiple.css';
 
+import '../assets/QuestionChoiceMultiple.css';
+
 export default function QuestionChoiceMultiple({ question_choice, correctOptionIndex, selectedQuestionId, isAnswered, onQuestionSelect }) {
   return (
     <div className="QuestionChoiceMultiple">
-      {question_choice.map((choice, index) => {
+      {question_choice.map((choice) => {
+        const { option_index, option_text } = choice;
+
         // Initialize a variable for the button's class
         let buttonClass = '';
 
         // Apply the 'correct-answer' class to the correct answer
-        if (index === correctOptionIndex) {
-          buttonClass = 'correct-answer'; // Apply the class to the correct answer
+        if (option_index === correctOptionIndex) {
+          buttonClass = 'correct-answer';
         }
 
-        // If the answer is incorrect, apply the 'incorrect-answer' class
-        if (selectedQuestionId !== null && correctOptionIndex !== null && selectedQuestionId === index && selectedQuestionId !== correctOptionIndex) {
-          buttonClass = 'incorrect-answer'; // Apply the 'incorrect-answer' class for an incorrect answer
+        // Apply the 'incorrect-answer' class if the selected answer is wrong
+        if (
+          selectedQuestionId !== null &&
+          correctOptionIndex !== null &&
+          selectedQuestionId === option_index &&
+          selectedQuestionId !== correctOptionIndex
+        ) {
+          buttonClass = 'incorrect-answer';
         }
 
         return (
-          <div key={index} className="choice">
+          <div key={option_index} className="choice">
             <button
-              onClick={() => onQuestionSelect(index)}
-              className={`${selectedQuestionId === index ? 'selected-button' : ''} ${buttonClass}`}
+              onClick={() => onQuestionSelect(option_index)}
+              className={`${selectedQuestionId === option_index ? 'selected-button' : ''} ${buttonClass}`}
               style={{
-                cursor: isAnswered ? 'not-allowed' : 'pointer', // Apply 'cursor: not-allowed' if the answer has been submitted
+                cursor: isAnswered ? 'not-allowed' : 'pointer',
               }}
-              disabled={isAnswered} // Disable the button once the answer is submitted
+              disabled={isAnswered}
             >
-              {choice}
+              {option_text}
             </button>
           </div>
         );
