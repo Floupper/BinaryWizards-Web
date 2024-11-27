216cd5bb20f916f7325cb223acea823ea61c011a
fortes <thomas.fortes@etu.unistra.fr>
2024-11-27 16:23:52 +0100

    style:add style on navbar

--- binarywizards-webapp/src/components/Navbar.js
+++ binarywizards-webapp/src/components/Navbar.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react'; // Importation de useState
 import { NavLink, useNavigate } from 'react-router-dom';
+import { EmojiProvider, Emoji } from "react-apple-emojis";
+import emojiData from "react-apple-emojis/src/data.json";
 import '../assets/Navbar.css';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false); 
   const token = localStorage.getItem('token');
   const navigate = useNavigate();
 
   const handleSignout = () => {
-    localStorage.removeItem('token'); 
-    navigate('/'); 
+    localStorage.removeItem('token');
+    navigate('/');
+  };
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen); 
   };
 
   return (
@@ -16,10 +23,28 @@ export default function Navbar() {
       <div className="Navbar-logo">
         <NavLink to="/" className={({ isActive }) => (isActive ? 'active-link' : '')}>Quiz</NavLink>
       </div>
-      <ul className="Navbar-links">
-        <li><NavLink to="/" className={({ isActive }) => (isActive ? 'active-link' : '')}>Home</NavLink></li>
-        <li><NavLink to="/join-quiz" className={({ isActive }) => (isActive ? 'active-link' : '')}>Join Quiz</NavLink></li>
-        <li><NavLink to="/create-quiz" className={({ isActive }) => (isActive ? 'active-link' : '')}>Create Quiz</NavLink></li>
+      <button className="Navbar-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+      <ul className={`Navbar-links ${isOpen ? 'open' : ''}`}>
+        <li>
+          <EmojiProvider data={emojiData}>
+            <Emoji name="house" width={20} />
+          </EmojiProvider>
+          <NavLink to="/" className={({ isActive }) => (isActive ? 'active-link' : '')}>Home</NavLink>
+        </li>
+        <li>
+          <EmojiProvider data={emojiData}>
+            <Emoji name="waving-hand" width={20} />
+          </EmojiProvider>
+          <NavLink to="/join-quiz" className={({ isActive }) => (isActive ? 'active-link' : '')}>Join Quiz</NavLink>
+        </li>
+        <li>
+          <EmojiProvider data={emojiData}>
+            <Emoji name="paintbrush" width={20} />
+          </EmojiProvider>
+          <NavLink to="/create-quiz" className={({ isActive }) => (isActive ? 'active-link' : '')}>Create Quiz</NavLink>
+        </li>
         {token ? (
           <div className="Navbar-links">
             <li>
