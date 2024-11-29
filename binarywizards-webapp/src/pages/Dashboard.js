1008b55da0d9b3f64408c38834e81606e1b5c212
Louis HERTSCHUH <louis.hertschuh@etu.unistra.fr>
2024-11-29 11:04:49 +0100

    fix: dashboard schlegel redirection

--- binarywizards-webapp/src/pages/Dashboard.js
+++ binarywizards-webapp/src/pages/Dashboard.js
@@ -5,42 +5,42 @@ import PlayedQuizCard from '../components/DashboardPlayedQuizCard.js';
 import CreatedQuizCard from '../components/CreatedQuizCard.js';
 
 export default function Dashboard() {
-    const [userQuizzes, setUserQuizzes] = useState([]);
-    const [playedQuizzes, setPlayedQuizzes] = useState([]);
-    const userId = localStorage.getItem('token');
-    useEffect(() => {
-        if (userId) {
-            DashboardService.getUserQuizzes(userId)
-                .then(data => setUserQuizzes(data))
-                .catch(error => console.error('Error fetching user quizzes:', error));
+  const [userQuizzes, setUserQuizzes] = useState([]);
+  const [playedQuizzes, setPlayedQuizzes] = useState([]);
+  const userId = localStorage.getItem('token');
+  useEffect(() => {
+    if (userId) {
+      DashboardService.getUserQuizzes(userId)
+        .then(data => setUserQuizzes(data))
+        .catch(error => console.error('Error fetching user quizzes:', error));
 
-            DashboardService.getPlayedQuizzes(userId)
-                .then(data => setPlayedQuizzes(data))
-                .catch(error => console.error('Error fetching played quizzes:', error));
-        }
-    }, [userId]);
+      DashboardService.getPlayedQuizzes(userId)
+        .then(data => setPlayedQuizzes(data))
+        .catch(error => console.error('Error fetching played quizzes:', error));
+    }
+  }, [userId]);
 
-    return (
-        <div className="dashboard-container">
-          <h1>Dashboard</h1>
-          <div className="quizzes-container">
-            <div className="created-quizzes">
-              <h2>Your Created Quizzes</h2>
-              <div className="quiz-list scrollable">
-                {userQuizzes.map((quiz) => (
-                  <CreatedQuizCard key={quiz.id} quiz={quiz} route={'/dashboard/detail-create-quiz/${quiz.id}'}/>
-                ))}
-              </div>
-            </div>
-            <div className="played-quizzes">
-              <h2>Your Played Quizzes</h2>
-              <div className="quiz-list scrollable">
-                {playedQuizzes.map((quiz) => (
-                  <PlayedQuizCard key={quiz.game_id} quiz={quiz} />
-                ))}
-              </div>
-            </div>
+  return (
+    <div className="dashboard-container">
+      <h1>Dashboard</h1>
+      <div className="quizzes-container">
+        <div className="created-quizzes">
+          <h2>Your Created Quizzes</h2>
+          <div className="quiz-list scrollable">
+            {userQuizzes.map((quiz) => (
+              <CreatedQuizCard key={quiz.id} quiz={quiz} route={`/dashboard/detail-create-quiz/${quiz.id}`} />
+            ))}
           </div>
         </div>
-      );
+        <div className="played-quizzes">
+          <h2>Your Played Quizzes</h2>
+          <div className="quiz-list scrollable">
+            {playedQuizzes.map((quiz) => (
+              <PlayedQuizCard key={quiz.game_id} quiz={quiz} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
