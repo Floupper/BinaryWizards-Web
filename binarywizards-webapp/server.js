d0855c9b229277c121878da5e17da1a13e4c86ad
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2024-12-04 10:32:09 +0100

    feat: add express to use as server

---
+++ binarywizards-webapp/server.js
@@ -0,0 +1,39 @@
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+// Servir les fichiers statiques de l'application React buildée
+app.use(express.static(path.join(__dirname, "build")));
+
+// Toute autre route redirige vers index.html (pour React Router)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const httpsPort = Number(process.env.PORT) || 33034;
+
+let sslOptions = {};
+if (process.env.APP_ENV === "server") {
+  try {
+    sslOptions = {
+      key: fs.readFileSync(process.env.key),
+      cert: fs.readFileSync(process.env.cert),
+    };
+    console.log("Certificats SSL chargés avec succès.");
+  } catch (err) {
+    console.error("Erreur lors du chargement des certificats SSL :", err);
+  }
+}
+
+if (process.env.APP_ENV === "server") {
+  https.createServer(sslOptions, app).listen(httpsPort, () => {
+    console.log(`🚀 Server running on https://localhost:${httpsPort}`);
+  });
+} else {
+  app.listen(httpsPort, "0.0.0.0", () => {
+    console.log(`Server is running on http://0.0.0.0:${httpsPort}`);
+  });
+}
