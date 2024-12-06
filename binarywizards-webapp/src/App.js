4a33d6b09d8cc6b08760886b3c468618143876b5
Louis HERTSCHUH <louis.hertschuh@etu.unistra.fr>
2024-12-06 09:55:57 +0100

    fix: fix toast placement on app.js

--- binarywizards-webapp/src/App.js
+++ binarywizards-webapp/src/App.js
@@ -19,26 +19,28 @@ import CreateQuizQuick from './pages/CreateQuizQuick.js';
 function App() {
 
   return (
-    <Router>
+    <>
+      <Router>
+
+        <Routes>
+          <Route path="/" element={<JoinQuiz />} />
+          <Route path="/create-quiz" element={<CreateQuiz />} />
+          <Route path="/edit-quiz" element={<CreateQuiz />} />
+          <Route path="/signin" element={<ConnectScreen />} />
+          <Route path="/signup" element={<SignupScreen />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/history-game-play/:quizId" element={<HistoryGamePlay />} />
+          <Route path="/dashboard/detail-create-quiz/:quizId" element={<QuizRecapScreen />} />
+          <Route path="/question" element={<Navigate to="/" replace />} />
+          <Route path="/question/:id" element={<QuestionScreen />} />
+          <Route path="/end" element={<EndScreen />} />
+          <Route path="/quiz-created/:quizId" element={<QuizCreated />} />
+          <Route path="/quick-quiz" element={< CreateQuizQuick />} />
+        </Routes>
+
+      </Router>
       <ToastContainer />
-      <Routes>
-        <Route path="/" element={<JoinQuiz />} />
-        <Route path="/create-quiz" element={<CreateQuiz />} />
-        <Route path="/edit-quiz" element={<CreateQuiz />} />
-        <Route path="/signin" element={<ConnectScreen />} />
-        <Route path="/signup" element={<SignupScreen />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/history-game-play/:quizId" element={<HistoryGamePlay />} />
-        <Route path="/dashboard/detail-create-quiz/:quizId" element={<QuizRecapScreen />} />
-        <Route path="/question" element={<Navigate to="/" replace />} />
-        <Route path="/question/:id" element={<QuestionScreen />} />
-        <Route path="/end" element={<EndScreen />} />
-        <Route path="/quiz-created/:quizId" element={<QuizCreated />} />
-        <Route path="/quick-quiz" element={< CreateQuizQuick />} />
-      </Routes>
-
-    </Router>
-
+    </>
   );
 }
 
