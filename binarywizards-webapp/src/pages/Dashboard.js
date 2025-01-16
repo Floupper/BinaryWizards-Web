d7c56dc996a10c5353ee3309ea33ef74893a112f
Louis Schlegel <louisschlegel@wifi-eduroam-interne-172-29-111-52.wifi.unistra.fr>
2025-01-16 16:40:12 +0100

    fix: dashboard

--- binarywizards-webapp/src/pages/Dashboard.js
+++ binarywizards-webapp/src/pages/Dashboard.js
@@ -23,11 +23,11 @@ export default function Dashboard() {
   }, [userId]);
 
   return (
-    <div className="min-h-screen bg-cover bg-center"
+    <div className="min-h-screen max-h-screen bg-cover bg-center"
       style={{ backgroundImage: "url('/backgrounds/JoinQuizBackground.svg')" }}
     > 
       <Navbar />
-      <div className="p-6">
+      <div className="px-6">
         <div className="flex space-x-4 mb-4 text-xl lg:text-2xl">
           <button
             className={`px-6 py-3 rounded-md hover:bg-black hover:text-white ${activeTab === 'created' ? 'bg-black text-white' : 'bg-white text-black'}`}
@@ -44,7 +44,7 @@ export default function Dashboard() {
             Your Played Quizzes
           </button>
         </div>
-        <div className="quiz-list max-h-[43.75rem] w-full overflow-y-auto">
+        <div className="quiz-list max-h-[38rem] w-full overflow-y-auto">
           {activeTab === 'created' && (
             <div className="justify-items-center items-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
               {userQuizzes.map((quiz) => (
